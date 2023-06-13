2074c6f58b8960fad21d29be8037de1d57a7ed3c
brDev <bekakrarabah@gmail.com>
2023-06-13 23:03:55 +0100

    Initialization of spring project

---
+++ src/main/java/com/brtech/springstudents/SpringAliBoualiApplication.java
@@ -0,0 +1,13 @@
+package com.brtech.springstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAliBoualiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAliBoualiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brtech/springstudents/SpringAliBoualiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brtech.springstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAliBoualiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
